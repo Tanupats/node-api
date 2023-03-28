54803592906fe7e8b9a1c86ebc95887ebbb17dd7
Tanupat Sittiwong <69186299+Tanupats@users.noreply.github.com>
2023-03-28 22:48:24 +0700

    api
    node-api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const books = require('./db')
 const app = express()
+// Create a router to handle routes
+const router = express.Router();
+const serverless = require("serverless-http");
 const PORT = process.env.PORT || 3050
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,20 +12,27 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+
+router.get('/', (req, res) => {
 
     res.send('hello world')
 });
 
-app.get('/members', (req, res) => {
+router.get('/members', (req, res) => {
     res.json({ books });
 })
 
-app.get('/member/:id', (req, res) => {
+router.get('/member/:id', (req, res) => {
 
     res.json(books.find(book => book.id === Number(req.params.id)))
 
 })
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use(`/.netlify/functions/api`, router);
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
 
 app.listen(PORT, () => {
     console.log(`Start server at port ${PORT}`)
