0c86445d58556034ed80f739d268c181ac0a11c5
Tanupat Sittiwong <69186299+Tanupats@users.noreply.github.com>
2023-03-28 22:52:20 +0700

    api
    test

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const books = require('./db')
 const app = express()
-// Create a router to handle routes
-const router = express.Router();
-const serverless = require("serverless-http");
-const PORT = process.env.PORT || 3050
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -12,29 +9,22 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
 
     res.send('hello world')
 });
 
-router.get('/members', (req, res) => {
+app.get('/members', (req, res) => {
     res.json({ books });
 })
 
-router.get('/member/:id', (req, res) => {
+app.get('/member/:id', (req, res) => {
 
     res.json(books.find(book => book.id === Number(req.params.id)))
 
 })
 
-// Use the router to handle requests to the `/.netlify/functions/api` path
-app.use(`/.netlify/functions/api`, router);
-// Export the app and the serverless function
-module.exports = app;
-module.exports.handler = serverless(app);
-
-app.listen(PORT, () => {
-    console.log(`Start server at port ${PORT}`)
+app.listen(3050, () => {
+    console.log('Start server at port 3050.')
 })
 
